ee49a518ccf0a8f2bbec0a97fe57b25b767fb5dc
Ashish Verma <ashish1500616@gmail.com>
2017-11-10 23:44:01 +0530

    Add Image Model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+
+class Image(models.Model):
+
+    img_1_a = models.ImageField(blank=True)
+    img_1_b = models.ImageField(blank=True)
+    img_two_a = models.ImageField(blank=True)
+    img_two_b = models.ImageField(blank=True)
+    img_3_a = models.ImageField(blank=True)
+    img_3_b = models.ImageField(blank=True)
